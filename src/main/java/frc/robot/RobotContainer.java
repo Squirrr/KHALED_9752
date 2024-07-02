335517bc57a2c94667e30cd5b127fa7c46f81019
Squirrr <abdullah.khaled.542@k12.friscoisd.org>
2024-07-02 10:12:01 -0500

    First Commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,33 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.PS5Controller;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandPS5Controller;
+import frc.robot.Commands.IntakeCommand;
+import frc.robot.Commands.OuttakeCommand;
+import frc.robot.Subsystems.IntakeSubsystem;
+
+public class RobotContainer {
+  public IntakeSubsystem intake = new IntakeSubsystem();
+  private final CommandPS5Controller base = new CommandPS5Controller(0);
+  public RobotContainer() {
+    
+    intake.setDefaultCommand(intake.intakeDefault());
+    configureBindings();
+  }
+
+  private void configureBindings() {
+
+    base.R1().whileTrue(new IntakeCommand(intake));
+    base.L1().whileTrue(new OuttakeCommand(intake));
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
